efb3c2eee7479101a3906cbb51b9064d78b88548
Chioke A. Johnson <cjohn30001@yahoo.com>
2014-08-12 22:57:18 -0400

    added pic to about page, added first visit footer

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,26 @@
 class WelcomeController < ApplicationController
   def news
   end
+  def index
+  	# get current date and time
+  	# create a string version 
+  	current_dt = Time.now.strftime("%B %d, %Y, %A, %H, %M")
+  	
+  	#store the current date and time in the session
+  	#
+  	if session['first_visit'] == nil
+  	session['first_visit'] == current_dt
+  end
+  	#pull the date and time from the session
+  	#make available to view
+
+  	@first_visit = session['first_visit']
+end
+
+
+
+  def about
+    @url = "NY-skyline.jpg" 
+  end
+
 end
